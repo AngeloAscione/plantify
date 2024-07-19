931ceba65afd18242ce26a91e349f35bf28d59c4
Angelo Ascione <a.ascione@asignt.it>
2024-07-20 01:30:27 +0200

    Inizio sviluppo PasswordTool

--- src/main/java/it/unisa/utils/PasswordTool.java
+++ src/main/java/it/unisa/utils/PasswordTool.java
@@ -7,6 +7,11 @@ import java.util.regex.Pattern;
 
 public class PasswordTool {
 
+    /**
+     * Metodo per validare una password che deve avere i seguenti criteri: Lungezza tra i gli 8 e 16 caratteri, almeno un carattere speciale, almeno un numero, almeno una lettera maiuscola
+     * @param password
+     * @return True se la password rispetta tutti i requisiti, false altrimenti
+     */
     public static boolean idValidPassword(String password) {
         if (password.length() < 8 || password.length() > 16){
             return false;
